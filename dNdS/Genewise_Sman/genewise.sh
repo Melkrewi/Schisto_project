941a4b8ed5a444956b4fb10eba7ed5284fe618d0
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-01-22 22:16:39 +0100

    Create genewise.sh

---
+++ dNdS/Genewise_Sman/genewise.sh
@@ -0,0 +1,23 @@
+module load java
+
+module load wise/2.4.1
+
+module load blat
+
+for (( i = 1; i <= 22; i++ ))
+
+do
+
+sed -n "$i"p  names_for_genewise.txt | cut -f 1 | sort | uniq | ~/seqtk/seqtk subseq Final_Set_W_candidates_Mansoni.fasta /dev/stdin > "$i"_w.fa
+
+sed -n "$i"p  names_for_genewise.txt | cut -f 2 | sort | uniq | /~/seqtk/seqtk subseq schistosoma_mansoni.PRJEA36577.WBPS14.protein.fa /dev/stdin > "$i"_prot.fa
+
+sed -n "$i"p  names_for_genewise.txt | cut -f 2 | sort | uniq | ~/seqtk/seqtk subseq schistosoma_mansoni.PRJEA36577.WBPS14.CDS_transcripts.fa /dev/stdin > "$i"_z.fa
+
+genewise -cdna "$i"_prot.fa "$i"_w.fa -both > "$i"_w.CDS
+
+genewise -cdna "$i"_prot.fa "$i"_z.fa -both > "$i"_z.CDS
+
+cat "$i"_w.CDS "$i"_z.fa > "$i"_final_cds.fasta
+
+done
