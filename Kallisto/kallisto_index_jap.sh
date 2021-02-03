1b0d5dc9e35a7e8de9dee695b4df7c721bd59f43
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-02-03 12:25:22 +0100

    Add files via upload

---
+++ Kallisto/kallisto_index_jap.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# OPTIONS
+
+#SBATCH --job-name="kallisto"
+#SBATCH --mail-user=picardm@obs-banyuls.fr
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=kallisto.out
+
+#SBATCH --mem=8G
+#SBATCH --time=24:00:00
+
+# MORE INFOS
+
+#-# last edit: 16-11-2020
+#-# usage: qsub kallisto_index_jap.sh 
+#-# prog required:  kallisto/0.43.1 
+#-# description: kallisto is a program for quantifying abundances of transcripts from bulk and single-cell RNA-Seq data, or more generally of target sequences using high-throughput sequencing reads.  It is based on the novel idea of pseudoalignment for rapidly determining the compatibility of reads with targets, without the need for alignment. 
+#-# more info here: https://pachterlab.github.io/kallisto/about.html
+
+### START
+
+TRANSCRIPTOME="/nfs/scistore03/vicosgrp/mpicard/NEW_expressionanalysis/schistosoma_japonicum_CDS_Kallisto_17_11_2020.fasta" ;
+OUF_INDEX="/nfs/scistore03/vicosgrp/mpicard/NEW_expressionanalysis/kallisto_index_japonicum";
+
+
+mkdir $OUF_INDEX
+
+module load  kallisto/0.43.1 
+
+# Create index
+
+kallisto index -i $OUF_INDEX/transcripts.idx $TRANSCRIPTOME
+
+### END
+
