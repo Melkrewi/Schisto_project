e181e7da90118e6709c17d7a390fcff8b64d44c8
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-05-03 15:00:02 +0200

    Update SOAPdenovo_trans_Sman.sh

--- Kmer_analysis/SOAPdenovo_trans_Sman.sh
+++ Kmer_analysis/SOAPdenovo_trans_Sman.sh
@@ -1,3 +1,4 @@
+echo "### Step 1: Assembly of Candidates"
 module load SOAPdenovoTrans
 
-srun SOAPdenovo-Trans-31mer all -s soapconfig_Smansoni.txt -K 15 -o TranscriptAssembly -p 16 1> TranscriptAssembly.stdout 2> TranscriptAssembly.stderr
+srun SOAPdenovo-Trans-31mer all -s soapconfig.txt -K 15 -o TranscriptAssembly -p 16 1> TranscriptAssembly.stdout 2> TranscriptAssembly.stderr
