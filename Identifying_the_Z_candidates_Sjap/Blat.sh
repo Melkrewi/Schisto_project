5a982a411d1eadce143b35b66fe6dabdd7b668f0
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-05-10 15:44:15 +0200

    Update Blat.sh

--- Identifying_the_Z_candidates_Sjap/Blat.sh
+++ Identifying_the_Z_candidates_Sjap/Blat.sh
@@ -1,7 +1,8 @@
-module load java
-module load blat
+echo "### Step 7: Finding the Z candidates"
 
-blat -minScore=50 Trinity_cap3.fasta Final_Set_W_Candidates_Japonicum.fasta W_vs_trinity.blat -t=dnax -q=dnax
+module load blat
+blat -minScore=50 Trinity_cap3.fasta final_w_japonicum.fasta W_vs_trinity.blat -t=dnax -q=dnax
 
 sort -k 10 W_vs_trinity.blat > W_vs_trinity.blat.sorted
+
 perl 2-besthitblat.pl W_vs_trinity.blat.sorted
