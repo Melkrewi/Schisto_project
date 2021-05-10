8fe9e427621935fc30207f931bac4a15382e657e
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-05-10 15:45:42 +0200

    Delete Blat.sh

--- Identifying_the_Z_candidates_Sjap/Blat.sh
+++
@@ -1,8 +0,0 @@
-echo "### Step 7: Finding the Z candidates"
-
-module load blat
-blat -minScore=50 Trinity_cap3.fasta final_w_japonicum.fasta W_vs_trinity.blat -t=dnax -q=dnax
-
-sort -k 10 W_vs_trinity.blat > W_vs_trinity.blat.sorted
-
-perl 2-besthitblat.pl W_vs_trinity.blat.sorted
