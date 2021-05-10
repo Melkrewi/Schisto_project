875345cf1ba2571e2f6461b854d70a511d38d3e2
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-05-10 15:28:30 +0200

    Update Trans_curation_for_OrthoFinder.sh

--- Transcriptome_curation/Sman_OrthoFinder/Trans_curation_for_OrthoFinder.sh
+++ Transcriptome_curation/Sman_OrthoFinder/Trans_curation_for_OrthoFinder.sh
@@ -1,33 +1,29 @@
-#merge the single CDS files from genewise:
+#merge the the single CDS to have a file with only coding Z and W to use for Orthofinder
+
 for file in ~/final_cds/*_final_cds.fasta
 
 do 
 
-cat $file >> Mansoni_ZW_CDS_09_11_2020.fasta
+cat $file >> japonicum_ZW_CDS.fasta
 
 done
 
-#collapse the transcriptome using SpliceFinder.pl
+#collapse schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa using SpliceFinder_2.pl
 
-perl SpliceFinder_2.pl schistosoma_mansoni.PRJEA36577.WBPS14.CDS_transcripts.fa
+perl SpliceFinder_2.pl schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa
 
-#remove the annotated candidates from Mansoni_ZW_CDS_09_11_2020.fasta
-
-#use blat to find the copies in the transcriptomes that need to be removed
 module load blat
-
-blat -minScore=31 schistosoma_mansoni.PRJEA36577.WBPS14.CDS_transcripts.fa.long Mansoni_ZW_CDS_09_11_2020_without_smp.fasta ZW_vs_trans.blat -t=dnax -q=dnax
-
-sort -k 10 ZW_vs_trans.blat > ZW_vs_trans.blat.sorted
-
-perl 2-besthitblat.pl ZW_vs_trans.blat.sorted
+#blat japonicum_ZW_CDS.fasta against the collapsed transcriptome
+blat -minScore=31 schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long japonicum_ZW_CDS_12_04_2021.fasta ZW_vs_trans.blat -t=dnax -q=dnax
 
 #filter for matches > 100 and & mismatch/(match+mismatch)<'0.05'
-
 cat ZW_vs_trans.blat | awk '($1>100 && ($2/($1+$2+0.00000001))<0.05)' > filtered_all.txt
 
-#remove all the transcripts in filtered_all from the collapsed transcriptome
-perl -ne 'if(/^>(\S+)/){$c=!$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' remove.txt schistosoma_mansoni.PRJEA36577.WBPS14.CDS_transcripts.fa.long > schistosoma_mansoni_clean_CDS_17_11_2020.fasta
+#remove all the transcripts in filtered_all from the collapsed transcriptome by listing all the transcripts in remove.txt and using the command below
 
-#add Mansoni_W_CDS_09_11_2020_without_smp_renamed.fasta to the clean collapsed transcriptome
-cat schistosoma_mansoni_clean_CDS_17_11_2020.fasta Mansoni_W_CDS_09_11_2020_without_smp_renamed.fasta > schistosoma_mansoni_plus_ZW_orthofinder_17_11_2020.fasta
+perl -ne 'if(/^>(\S+)/){$c=!$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' remove.txt schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long > schistosoma_japonicum_clean_CDS.fasta
+
+#add japonicum_ZW_CDS.fasta to the clean collapsed transcriptome
+
+cat schistosoma_japonicum_clean_CDS.fasta japonicum_ZW_CDS.fasta > schistosoma_japonicum_plus_ZW_orthofinder.fasta
+
