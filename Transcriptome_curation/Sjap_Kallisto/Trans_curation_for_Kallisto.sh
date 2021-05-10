13055b77e050f31dabdafa25695d6d55ff370e0b
Melkrewi <75329372+Melkrewi@users.noreply.github.com>
2021-05-10 15:14:39 +0200

    Update Trans_curation_for_Kallisto.sh

--- Transcriptome_curation/Sjap_Kallisto/Trans_curation_for_Kallisto.sh
+++ Transcriptome_curation/Sjap_Kallisto/Trans_curation_for_Kallisto.sh
@@ -1,16 +1,18 @@
 #concatenate the final z and w candidates.
-cat Final_Set_W_Candidates_Japonicum.fasta Final_Set_Z_Candidates_Japonicum_17_11_2020.fasta > Final_Set_ZW_Candidates_Japonicum_17_11_2020.fasta
+cat Final_Set_W_Candidates_japonicum.fasta Final_Set_Z_Candidates_japonicum.fasta > Final_Set_ZW_Candidates_japonicum.fasta
 
 #use blat to find the copies in the transcriptomes that need to be removed
 module load java
 module load blat
-blat -minScore=31 schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long Final_Set_ZW_Candidates_Japonicum_17_11_2020.fasta ZW_vs_clean_trans.blat -t=dnax -q=dnax
+blat -minScore=31 schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long Final_Set_ZW_Candidates_japonicum.fasta ZW_vs_clean_trans.blat -t=dnax -q=dnax
 
 #filter for matches > 100 and & mismatch/(match+mismatch)<'0.05'
 cat ZW_vs_clean_trans.blat | awk '($1>100 && ($2/($1+$2+0.00000001))<0.05)' > filtered_all.txt
 
-#remove all the transcripts in filtered_all from the collapsed transcriptome using
-perl -ne 'if(/^>(\S+)/){$c=!$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' remove.txt schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long > schistosoma_japonicum_clean_CDS_17_11_2020.fasta
+#remove all the transcripts in filtered_all from the collapsed transcriptome by listing all the transcripts in remove.txt and using the command below
 
-#add the Final_Set_ZW_Candidates_Japonicum_17_11_2020.fasta to the clean collapsed transcriptome
-cat schistosoma_japonicum_clean_CDS_17_11_2020.fasta Final_Set_ZW_Candidates_Japonicum_17_11_2020.fasta > schistosoma_japonicum_CDS_Kallisto_17_11_2020.fasta
+perl -ne 'if(/^>(\S+)/){$c=!$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' remove.txt schistosoma_japonicum.PRJEA34885.WBPS14.CDS_transcripts.fa.long > schistosoma_japonicum_clean_CDS.fasta
+
+#add the Final_Set_ZW_Candidates_japonicum.fasta to the clean collapsed transcriptome
+cat schistosoma_japonicum_clean_CDS.fasta Final_Set_ZW_Candidates_japonicum.fasta > schistosoma_japonicum_CDS_Kallisto.fasta
+
